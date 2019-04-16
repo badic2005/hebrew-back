dffc905095f7bf72c47e40762ced3142ce58af50
badic <vadim021703@gmail.com>
2019-04-16 10:13:07 +0300

    sdfsdf

--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import { MongoClient } from "mongodb";
 import WordsDAO from "./dao/wordsDAO";
 
 const port = process.env.PORT || 8000;
+const url = 'mongodb://root:r00tr00t@ds115523.mlab.com:15523/hebdict';
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
-//
-// MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-//   .catch(err => {
-//     console.error(err.stack);
-//     process.exit(1);
-//   })
-//   .then(async client => {
-//     await WordsDAO.injectDB(client);
-//     app.listen(port, () => {
-//       console.log(`listening on port ${port}`);
-//     });
-//   });
+MongoClient.connect(url, { useNewUrlParser: true })
+  .catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async client => {
+    await WordsDAO.injectDB(client);
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  });
