9665fb2cc6a0ee73a5ae116b1eab3a716a3d6d9d
badic <vadim021703@gmail.com>
2019-04-16 09:58:09 +0300

    fix

--- src/api/words.controller.js
+++ src/api/words.controller.js
@@ -2,11 +2,10 @@ import WordsDAO from "../dao/wordsDAO";
 
 export default class WordsController {
   static async apiGetWords(req, res, next) {
-   // const wordsList = await WordsDAO.getWords();
+    const wordsList = await WordsDAO.getWords();
 
     let response = {
-      'hello':'world'
-      //words: wordsList
+      words: wordsList
       // page: 0,
       // filters: {},
       // entries_per_page: MOVIES_PER_PAGE,
